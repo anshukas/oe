82d73dd069747324d8dc9f5dfbc57a788c7b5d69
anshukas <anshubtechinit@gmail.com>
2017-05-02 19:53:21 +0530

    static member can't access

---
+++ oe/src/oe/tmp/RF.java
@@ -0,0 +1,10 @@
+package oe.tmp;
+
+public class RF {
+	void display(){
+		System.out.println("Hey m display");
+	}
+	public static void main(String[] args) {
+		// display();  Cannot make a static reference to the non-static method display() from the type RF
+	}
+}
